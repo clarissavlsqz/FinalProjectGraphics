192948cfb7e73ea87b4976e80ac56e3adcc61a8a
clarissavlsqz <clarissa.a.velasquez@gmai.com>
2021-10-14 23:14:41 -0500

    create grass

--- js/main.js
+++ js/main.js
@@ -16,25 +16,36 @@ function init() {
     document.body.appendChild(renderer.domElement);
 
     // Green cube
-    const geometry = new THREE.BoxGeometry( 2, 2, 2 );
+    //const geometry = new THREE.BoxGeometry( 2, 2, 2 );
     //const material = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
-    const texture = new THREE.TextureLoader().load('textures/grass.png');
-    const material = new THREE.MeshBasicMaterial( {map: texture} );
-    cube = new THREE.Mesh( geometry, material );
-    scene.add(cube);
+    //const texture = new THREE.TextureLoader().load('textures/grass.png');
+    //const material = new THREE.MeshBasicMaterial( {map: texture} );
+    //cube = new THREE.Mesh( geometry, material );
+    //scene.add(cube);
 
-    camera.position.z = 5;
+    //Plane grass
+    const geometry = new THREE.PlaneGeometry(15, 10);
+    //const texture = new THREE.TextureLoader().load('textures/grass.png');
+    //const material = new THREE.MeshBasicMaterial( {map: texture, side: THREE.DoubleSide} );
+    const material = new THREE.MeshBasicMaterial( {color: 0x228B22} )
+    const floor = new THREE.Mesh( geometry, material );
+	floor.material.side = THREE.DoubleSide;
+	floor.rotation.x = 90;
+	scene.add( floor ); 
+
+
+    camera.position.z = 10;
 }
 
 
 
 function animate() {
     requestAnimationFrame(animate);
-    cube.rotation.x += 0.01
-    cube.rotation.y += 0.01
+    //cube.rotation.x += 0.01
+    //cube.rotation.y += 0.01
     renderer.render(scene, camera);
 }
-
+// Make the scene persisten when resize the window
 function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
