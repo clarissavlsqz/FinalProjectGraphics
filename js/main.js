91f5282d44a864c533c2e90920ddce58d604e200
clarissavlsqz <clarissa.a.velasquez@gmai.com>
2021-10-15 00:59:50 -0500

    added camera movement

--- js/main.js
+++ js/main.js
@@ -5,18 +5,31 @@ function init() {
     scene = new THREE.Scene();
 
     // Create camera
-    camera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 0.01, 1000);
+    camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.01, 2000);
+    camera.position.set(10,20,10);
 
     // Create render var
     renderer = new THREE.WebGLRenderer({antialias: true});
 
+    // Create controls var for camera movement
+    const controls = new THREE.OrbitControls(camera, renderer.domElement);
+    controls.update();
+
     // Set window size
     renderer.setSize(window.innerWidth, window.innerHeight);
-
     document.body.appendChild(renderer.domElement);
 
+    window.addEventListener('resize', function() {
+        var width = window.innerWidth;
+        var height = window.innerHeight;
+        renderer.setSize(width, height);
+        camera.aspect = width/height;
+        camera.updateProjectionMatrix;
+    });
+
+
     // Green cube
-    //const geometry = new THREE.BoxGeometry( 2, 2, 2 );
+    //const geometry = new THREE.BoxGeometry();
     //const material = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
     //const texture = new THREE.TextureLoader().load('textures/grass.png');
     //const material = new THREE.MeshBasicMaterial( {map: texture} );
@@ -24,25 +37,25 @@ function init() {
     //scene.add(cube);
 
     //Plane grass
-    const geometryPlane = new THREE.PlaneGeometry(15, 10);
+    const geometryPlane = new THREE.PlaneBufferGeometry(15,20);
+    geometryPlane.rotateX(4.7);
     //const texture = new THREE.TextureLoader().load('textures/grass.png');
     //const material = new THREE.MeshBasicMaterial( {map: texture, side: THREE.DoubleSide} );
     const materialPlane = new THREE.MeshBasicMaterial( {color: 0x228B22} )
     const floor = new THREE.Mesh( geometryPlane, materialPlane );
 	floor.material.side = THREE.DoubleSide;
-	floor.rotation.x = 90;
-	scene.add( floor ); 
+	scene.add(floor); 
 
     // Camping tent
     const geometryTent = new THREE.CylinderGeometry(0, 4, 5, 4, 1)
     const materialTent = new THREE.MeshNormalMaterial();
     const pyramid = new THREE.Mesh(geometryTent, materialTent);
     pyramid.position.x += 3;
-    pyramid.position.y += 2.5;
+    pyramid.position.y += 2.6;
     scene.add(pyramid);
 
 
-    camera.position.z = 10;
+    
 }
 
 
@@ -52,13 +65,13 @@ function animate() {
     //cube.rotation.x += 0.01
     //cube.rotation.y += 0.01
     renderer.render(scene, camera);
+    controls.update();
 }
 // Make the scene persisten when resize the window
-function onWindowResize() {
+/* function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
-}
-window.addEventListener('resize', onWindowResize, false);
+} */
 init();
 animate();
